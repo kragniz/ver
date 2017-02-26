b49656d1de54d498f1703a3fcebb6bd158d2b6f0
Louis Taylor <louis@kragniz.eu>
2017-02-26 06:32:52 +0000

    Handle some more cases

--- main.go
+++ main.go
@@ -8,7 +8,30 @@ import (
 )
 
 func handleFunc(f *types.Func) {
-	fmt.Println("func", f)
+	fmt.Println("Func:", f.Name())
+}
+
+func handleVar(v *types.Var) {
+	fmt.Println("Var:", v.Name(), v.Type())
+}
+
+func handleConst(c *types.Const) {
+	fmt.Println("Const:", c.Name())
+}
+
+func handleTypeName(t *types.TypeName) {
+	fmt.Println("TypeName:", t.Name(), t.Type())
+	switch t.Type().Underlying().(type) {
+	case *types.Struct:
+		fmt.Println("Struct!", t)
+		s := t.Type().Underlying().(*types.Struct)
+		for i := 0; i < s.NumFields(); i++ {
+			v := s.Field(i)
+			handleVar(v)
+		}
+	default:
+		fmt.Println("dunno")
+	}
 }
 
 func main() {
@@ -28,10 +51,27 @@ func main() {
 			obj := scope.Lookup(elem)
 			switch obj.(type) {
 
+			/*Object = *Func  // function, concrete method, or abstract method
+			  | *Var          // variable, parameter, result, or struct field
+			  | *Const        // constant
+			  | *TypeName     // type name
+			  | *Label        // statement label
+			  | *PkgName      // package name, e.g. json after import "encoding/json"
+			  | *Builtin      // predeclared function such as append or len
+			  | *Nil          // predeclared nil */
+
 			case *types.Func:
 				handleFunc(obj.(*types.Func))
 			case *types.Var:
-				fmt.Println("var", obj.Name(), obj.Type())
+				handleVar(obj.(*types.Var))
+			case *types.Const:
+				handleConst(obj.(*types.Const))
+			case *types.TypeName:
+				handleTypeName(obj.(*types.TypeName))
+			case *types.Label, *types.PkgName, *types.Builtin, *types.Nil:
+				// unimplemented
+				fmt.Println("Warning,", obj.Type(), "is unimplemented")
+				continue
 			default:
 				fmt.Println("not sure what it is", obj)
 			}
